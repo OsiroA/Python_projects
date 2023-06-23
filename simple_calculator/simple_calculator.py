96aee8d169c49c77f3e9bf62c497a3226da3b44d
OsiroA <124213013+OsiroA@users.noreply.github.com>
2023-06-23 11:16:41 +0100

    Update simple_calculator.py
    Added the logarithm function

--- simple_calculator/simple_calculator.py
+++ simple_calculator/simple_calculator.py
@@ -2,37 +2,36 @@
 """a simple calculator
 this is created as a personal project to test my skills
 """
-import math as d
+import math
 
 
-a = input("Please input the first number: ")
-op = input("Please enter the function you want to perform\
-            + to add, - to subtract, * to multiply, / to divide,\
-            ** to raise to power, | for square root: \n")
-b = input("please input the second number: ")
-a = float(a)
-b = float(b)
+def calculator(a, op, b=None):
+    symList = ["+", "-", "*", "/", "**", "|", "log"]
 
-symList = ["+", "-", "*", "/", "**", "|"]
+    if not isinstance(a, (int, float)):
+        print(f"Invalid number {a}, please check the number and try again.")
+        return
 
-if not isinstance(a, (int, float)):
-    print(f"Invalid number {a}, please check the number and try again")
-if not isinstance(b, (int, float)):
-    print(f"Invalid number {b}, please try again")
-if op not in symList:
-    print("Please check the operating symbol(function) and try again")
+    if op not in symList:
+        print("Please check the operating symbol (function) and try again.")
+        return
 
+    try:
+        if op == "+":
+            return a + b
+        elif op == "-":
+            return a - b
+        elif op == "*":
+            return a * b
+        elif op == "/":
+            return a / b
+        elif op == "**":
+            return a ** b
+        elif op == "|":
+            return math.sqrt(a)
+        elif op == "log":
+            return math.log(a)
 
-if op == "+":
-    print(a + b)
-if op == "-":
-    print(a - b)
-if op == "*":
-    print(a * b)
-if op == "/":
-    print(a / b)
-if op == "**":
-    print(a ** b)
-if op == "|":
-    print(d.sqrt(a), d.sqrt(b))
-
+    except TypeError:
+        print("Error: Missing or invalid value for the second number.")
+        return
