5ee5565959f771a4700d92bd5c35ac4c46c80e0c
OsiroA <osiroasunde@gmail.com>
2023-06-23 04:03:14 -0700

    Updasted simple-calculator: can now perform functions like root, trigonometry etc

--- simple_calculator/simple_calculator.py
+++ simple_calculator/simple_calculator.py
@@ -6,7 +6,7 @@ import math
 
 
 def calculator(a, op, b=None):
-    symList = ["+", "-", "*", "/", "**", "|", "log"]
+    symList = ["+", "-", "*", "/", "**", "root", "log", "sin", "cos", "tan"]
 
     if not isinstance(a, (int, float)):
         print(f"Invalid number {a}, please check the number and try again.")
@@ -27,11 +27,29 @@ def calculator(a, op, b=None):
             return a / b
         elif op == "**":
             return a ** b
-        elif op == "|":
+        elif op == "root":
             return math.sqrt(a)
         elif op == "log":
             return math.log(a)
+        elif op == "sin":
+            return math.sin(a)
+        elif op == "cos":
+            return math.cos(a)
+        elif op == "tan":
+            return math.tan(a)
 
     except TypeError:
         print("Error: Missing or invalid value for the second number.")
         return
+
+"""Prompt the user for input"""
+a = float(input("Enter your first number: "))
+op = input("Enter one of these (+, -, *, /, **, log, sin, cos, tan, root): ")
+b_in = input("Enter the second number or leave blank if not applicable: ")
+
+if b_in:
+    b = float(b_in)
+    solution = calculator(a, op, b)
+else:
+    solution = calculator(a, op)
+print(f"The Result of what you entered is: {solution}")
