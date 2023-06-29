1c61ebac71aa813b3fc32893a103d644e5bf55bf
OsiroA <osiroasunde@gmail.com>
2023-06-29 10:12:41 -0700

    Word Countrr Created

---
+++ word-counter/readme.txt
@@ -0,0 +1,15 @@
+s otherwise noted in the documents stored herein, my intent
+is for this material to be Creative Commons Attribution 2.5.
+
+I am very open for this material to be copyied and redistributed
+if you need particular permission - just send me E-mail.
+
+Charles Severance
+Fri Apr  6 13:55:08 EDT 2007Unless otherwise noted in the documents stored herein, my intent
+is for this material to be Creative Commons Attribution 2.5.
+
+I am very open for this material to be copyied and redistributed
+if you need particular permission - just send me E-mail.
+
+Charles Severance
+Fri Apr  6 13:55:08 EDT 2007
---
+++ word-counter/word-counter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This file counts the nunber of words in a text file
+and tells us the most occuring word snd the jumber of times it occured"""
+
+
+from collections import Counter
+
+
+nameoftextfile = input("Please enter the name of the file to be read: ")
+nameoftextfiletxt = nameoftextfile + ".txt"
+with open(nameoftextfiletxt, "r") as file:
+    contents = file.read()
+    words = contents.split()
+
+    """Now you can use the 'words' list for further processing"""
+
+    word_list = []
+    for word in words:
+        word_list.append(word)
+        word_counts = Counter(word_list)
+        sorted_counts = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
+        most-occuring = sorted_counts[0]
+        print(f"The most occuring word is {most-occuring[0]} abd it appears {most-occuring[1]} times")
